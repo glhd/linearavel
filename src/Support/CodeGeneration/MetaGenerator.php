5245f626bad441388b251cd162d7f2b31d12713a
Chris Morrell <inxilpro@users.noreply.github.com>
2024-03-08 22:33:50 -0500

    WIP

--- src/Support/CodeGeneration/MetaGenerator.php
+++ src/Support/CodeGeneration/MetaGenerator.php
@@ -3,29 +3,12 @@
 namespace Glhd\Linearavel\Support\CodeGeneration;
 
 use Glhd\Linearavel\Support\KeyHelper;
-use GraphQL\Language\AST\DefinitionNode;
-use GraphQL\Language\AST\EnumTypeDefinitionNode;
-use GraphQL\Language\AST\EnumValueDefinitionNode;
-use GraphQL\Language\AST\InterfaceTypeDefinitionNode;
-use GraphQL\Language\AST\ObjectTypeDefinitionNode;
-use GraphQL\Language\Parser;
-use Illuminate\Support\Arr;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name;
-use PhpParser\Node\Scalar\String_;
-use PhpParser\Node\Stmt\Enum_;
-use PhpParser\Node\Stmt\EnumCase;
-use PhpParser\Node\Stmt\Interface_;
-use PhpParser\Node\Stmt\Namespace_;
-use PhpParser\ParserFactory;
-use PhpParser\PrettyPrinter\Standard;
-use RuntimeException;
-use Spatie\LaravelData\Data;
 
 class MetaGenerator
 {
 	public function __construct(
 		protected string $root,
+		protected string $method,
 	) {
 	}
 	
@@ -36,6 +19,17 @@ class MetaGenerator
 			->map(fn($key) => var_export($key, true))
 			->implode(', ');
 		
-		// dd($keys);
+		return <<<PHP
+		expectedArguments(
+			\Glhd\Linearavel\Support\Client::{$this->method}(),
+			0,
+			{$keys}
+		);
+		expectedArguments(
+			\Glhd\Linearavel\Facades\Linear::{$this->method}(),
+			0,
+			{$keys}
+		);
+		PHP;
 	}
 }
--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -4,17 +4,15 @@ namespace Glhd\Linearavel\Tests\Feature;
 
 use Glhd\Linearavel\Data\Team;
 use Glhd\Linearavel\Facades\Linear;
-use Glhd\Linearavel\Support\Client;
 use Glhd\Linearavel\Support\CodeGeneration\MetaGenerator;
 use Glhd\Linearavel\Support\CodeGeneration\Transformer;
 use Glhd\Linearavel\Tests\TestCase;
-use Illuminate\Support\Facades\Http;
 
 class ClientTest extends TestCase
 {
 	public function test_it_can_fetch_teams(): void
 	{
-		Linear::teams('name', 'icon', 'key')->dd();
+		Linear::teams('name', 'icon', 'key');
 	}
 	
 	public function test_meta_generator(): void
@@ -27,10 +25,7 @@ class ClientTest extends TestCase
 	{
 		$transformer = new Transformer(__DIR__.'/../../local.graphql', 'Glhd\\Linearavel\\');
 		// echo $transformer->write();
+		// ->first()
+		// 			->organization->users->nodes->first()->name
 	}
-}
-
-enum Foo: string
-{
-	case bar = 'bar';
 }
