1841707aa500a52490daa87d7dd06a34fd56b03f
Chris Morrell <inxilpro@users.noreply.github.com>
2024-03-17 22:11:40 -0400

    Add helper function

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,7 +1,10 @@
 <?php
 
-/*
-if (!function_exists('')) { // @codeCoverageIgnore
+use Glhd\Linearavel\Connectors\LinearConnector;
 
+if (! function_exists('linear')) { // @codeCoverageIgnore
+	function linear(): LinearConnector
+	{
+		return app(LinearConnector::class);
+	}
 }
-*/
