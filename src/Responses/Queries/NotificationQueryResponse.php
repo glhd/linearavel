65075f6994e4365ad0c53d28bd1810e87923a6eb
Chris Morrell <inxilpro@users.noreply.github.com>
2024-06-27 11:09:08 -0400

    WIP

--- src/Responses/Queries/NotificationQueryResponse.php
+++ src/Responses/Queries/NotificationQueryResponse.php
@@ -2,13 +2,19 @@
 
 namespace Glhd\Linearavel\Responses\Queries;
 
+use Glhd\Linearavel\Contracts\SkipsCodeGeneration;
 use Glhd\Linearavel\Data\Contracts\Notification;
+use Glhd\Linearavel\Data\IssueNotification;
+use Glhd\Linearavel\Data\OauthClientApprovalNotification;
+use Glhd\Linearavel\Data\ProjectNotification;
 use Glhd\Linearavel\Responses\LinearResponse;
+use RuntimeException;
 
-class NotificationQueryResponse extends LinearResponse
+class NotificationQueryResponse extends LinearResponse implements SkipsCodeGeneration
 {
-	public function resolve(): Notification
+	public function resolve(): IssueNotification|ProjectNotification|OauthClientApprovalNotification
 	{
-		return Notification::from($this->json('data.notification'));
+		// return Notification::from($this->json('data.notification'));
+		throw new RuntimeException('Not yet implemented.');
 	}
 }
